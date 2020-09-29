38c88a7c44051395761f2feaefe619493ab1a0e4
ByNoMeans <barrettruthtots@gmail.com>
2020-09-28 23:34:44 -0500

    finish file io lab

--- file-io/main.cpp
+++ file-io/main.cpp
@@ -3,26 +3,75 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+void skipCells(stringstream &in, string &filler, const int &count) {
+    for (int i{}; i < count; i++)
+        getline(in, filler, ',');
+}
+
+bool sortVPData = [](
+        const pair<string, double> &a,
+        const pair<string, double> &b
+        ) { return a.second > b.second; };
 
 int main() {
 
-    std::fstream in { "WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv" };
-    std::string line;
-    constexpr int skip = 17;
+    fstream in{"WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv"};
 
     if (!in.is_open())
-        std::exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
+
+    // NOTICE: start on line 28, excluding development, income, world; could be source of misalignments
+    // Debug with debugger/printing out
+    // Also make sure getting right columns
+    constexpr int skip = 28;
+    constexpr int countryAgeIndex = 14;
+    constexpr int countryNameIndex = 3;
+    const vector developedCountryNames{"EUROPE", "NORTHERN AMERICA", "Japan", "Australia/New Zealand",
+                                       "United States of America"};
+
+    vector<pair<string, double>> developedCountryData{};
+    vector<pair<string, double>> countryData{};
+    string line{};
 
     for (int i = 0; i < skip; i++)
-        std::getline(in, line);
+        getline(in, line);
 
-    while (std::getline(in, line)) {
+    while (getline(in, line)) {
+        stringstream iss{line};
+        string countryName{};
+        double countryAge{};
 
-        std::stringstream iss { line };
-        std::string each;
+        skipCells(iss, line, countryNameIndex - 1);
+        getline(iss, line, ',');
+        countryName = line.substr(1, line.length() - 2);
 
-        while (std::getline(iss, each, ','))
-            std::cout << each << " ";
-        std::cout << std::endl;
+
+        skipCells(iss, line, countryAgeIndex - 1);
+        getline(iss, line, ',');
+        countryAge = stod(line);
+
+        if (find(developedCountryNames.begin(), developedCountryNames.end(), countryName) !=
+            developedCountryNames.end())
+            developedCountryData.emplace_back(countryName, countryAge);
+        countryData.emplace_back(countryName, countryAge);
+
+        iss.str(string());
     }
+    in.close();
+
+    sort(countryData.begin(), countryData.end(), sortVPData);
+    sort(developedCountryData.begin(), developedCountryData.end(), sortVPData);
+
+    for (int i = 0; i < countryData.size(); i++)
+        if (countryData.at(i).first == "United States of America")
+            cout << "The U.S. Ranks " << i + 1 << "th in life expectancy overall."
+                 << countryData.at(i).second << "\n";
+
+    for (int i = 0; i < developedCountryData.size(); i++)
+        if (developedCountryData.at(i).first == "United States of America")
+            cout << "The U.S. Ranks " << i + 1 << "th in life expectancy with respect to developed countries." << "\n";
 }
