c6f41a417eda5b7cc459db397b3cbaa0a05edcdd
ByNoMeans <barrettruthtots@gmail.com>
2020-09-30 18:56:15 -0500

    file io altered solution

--- file-io/main.cpp
+++ file-io/main.cpp
@@ -17,25 +17,34 @@ string skipCellsReturnNext(const string& in, const int& stop) {
 
 int main() {
 
-    fstream in{"WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv"};
+    fstream in{"../WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv"};
 
     if (!in.is_open() || in.bad())
         exit(EXIT_FAILURE);
 
-    const vector<string> developedCountryNames{"\"Japan\"", "\"EUROPE\"", "\"NORTHERN AMERICA\"", "\"Australia/New Zealand\"", "\"United States of America\""};
+    const vector<string> developedCountryNames{"\"Japan\"", "\"EUROPE\"", "\"NORTHERN AMERICA\"", "\"Australia/New Zealand\""};
     vector<pair<string, double>> developedCountryData{};
-    vector<pair<string, double>> countryData{};
+    vector<pair<string, double>> allData{};
     string line{};
+    bool isDeveloped{};
+    bool isRegion{};
 
     for (int i{}; i < 29; i++) getline(in, line);
 
     while (getline(in, line)) {
         string countryName = skipCellsReturnNext(line, 2);
         double countryAge = stod(skipCellsReturnNext(line, 15));
-        countryData.emplace_back(countryName, countryAge);
-        if (find(developedCountryNames.begin(), developedCountryNames.end(), countryName) != developedCountryNames.end())
+        isRegion = all_of(countryName.begin(), countryName.end(), [](char c){ return std::isupper(c) || c == '"'; });
+        if (isRegion) {
+            isDeveloped = find(developedCountryNames.begin(), developedCountryNames.end(), countryName) != developedCountryNames.end()
+                    || countryName == "\"United States of America\"";
+            getline(in, line);
+        }
+        if (isDeveloped)
             developedCountryData.emplace_back(countryName, countryAge);
+        allData.emplace_back(countryName, countryAge);
     }
+
     in.close();
 
     auto sortVP = [](
@@ -43,15 +52,15 @@ int main() {
             const pair<string, double> &b
     ) { return a.second > b.second; };
 
-    sort(countryData.begin(), countryData.end(), sortVP);
+    sort(allData.begin(), allData.end(), sortVP);
     sort(developedCountryData.begin(), developedCountryData.end(), sortVP);
 
-    for (int i = 0; i < countryData.size(); i++)
-        if (countryData.at(i).first == "\"United States of America\"")
-            printf("The US is rank #%d of all developed regions in life expectancy.\n", i+1);
+    for (int i = 0; i < allData.size(); i++)
+        if (allData.at(i).first == "\"United States of America\"")
+            printf("The US is rank #%d of all regions in life expectancy.\n", i+1);
 
     for (int i = 0; i < developedCountryData.size(); i++)
         if (developedCountryData.at(i).first == "\"United States of America\"")
-            printf("The US is rank #%d of all regions in life expectancy.\n", i+1);
+            printf("The US is rank #%d of all developed regions in life expectancy.\n", i+1);
 
 }
