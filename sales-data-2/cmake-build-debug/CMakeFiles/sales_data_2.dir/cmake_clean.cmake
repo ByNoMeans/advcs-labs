5e33e9dd314dbd646edb51d15ebb7883241ee693
ByNoMeans <barrettruthtots@gmail.com>
2020-10-08 16:51:22 -0500

    init sales data 2

---
+++ sales-data-2/cmake-build-debug/CMakeFiles/sales_data_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sales_data_2.dir/main.cpp.obj"
+  "libsales_data_2.dll.a"
+  "sales_data_2.exe"
+  "sales_data_2.exe.manifest"
+  "sales_data_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sales_data_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
