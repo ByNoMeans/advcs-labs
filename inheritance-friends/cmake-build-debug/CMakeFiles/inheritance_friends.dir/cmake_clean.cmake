3d4c635cc44194ced758bacd9a55e07b0811649e
ByNoMeans <barrettruthtots@gmail.com>
2020-10-21 14:34:44 -0500

    init inheritance and friends lab

---
+++ inheritance-friends/cmake-build-debug/CMakeFiles/inheritance_friends.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inheritance_friends.dir/main.cpp.obj"
+  "inheritance_friends.exe"
+  "inheritance_friends.exe.manifest"
+  "inheritance_friends.pdb"
+  "libinheritance_friends.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inheritance_friends.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ inheritance-friends/cmake-build-debug/CMakeFiles/inheritance_friends.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for inheritance_friends.
+# This may be replaced when dependencies are built.
