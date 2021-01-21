1c8f3d74d73e8a271e701fb1b35786f4b99a4755
ByNoMeans <barrettruthtots@gmail.com>
2021-01-21 15:07:22 -0600

    reset forced q push (oops)

---
+++ towers-of-hanoi/cmake-build-debug/CMakeFiles/towers_of_hanoi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/towers_of_hanoi.dir/main.cpp.obj"
+  "CMakeFiles/towers_of_hanoi.dir/stack.cpp.obj"
+  "libtowers_of_hanoi.dll.a"
+  "towers_of_hanoi.exe"
+  "towers_of_hanoi.exe.manifest"
+  "towers_of_hanoi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/towers_of_hanoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
